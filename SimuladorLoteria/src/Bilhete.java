7c4cdd06862376a3d48a3370ab4ddd2467bdd507
alvarofgomes <alvarogomes098gmail.com>
2025-08-20 15:11:17 -0300

    Simulador de loteria

--- SimuladorLoteria/src/Bilhete.java
+++ SimuladorLoteria/src/Bilhete.java
@@ -26,7 +26,36 @@ public class Bilhete {
 	}
 	
 	public void realizarSorteio() {
+		Random random = new Random();
 		
+		resultadoDoSorteio = new int[6];
+		
+		for(int i =0; i < resultadoDoSorteio.length; i++) {
+			resultadoDoSorteio[i] = random.nextInt(60) + 1;
+		}
+		Arrays.sort(resultadoDoSorteio);
+	}
+	
+	public int contarAcertos() {
+		int acertos = 0;
+		for(int numerosEscolhido : numerosEscolhidos) {
+			for(int numeroSorteado : resultadoDoSorteio) {
+				if(numerosEscolhido == numeroSorteado) {
+					acertos++;
+				}
+			}
+		}
+		return acertos;
+	}
+	
+	public void exibirResultados() {
+		System.out.println("Números escolhidos: " + Arrays.toString(numerosEscolhidos));
+		System.out.println("Números sorteados: " + Arrays.toString(resultadoDoSorteio));
+		
+		int acertos = contarAcertos();
+		
+		System.out.println(acertos + " Acertos. ");
+		System.out.println();
 	}
 	
 }
--- SimuladorLoteria/src/Main.java
+++ SimuladorLoteria/src/Main.java
@@ -12,12 +12,28 @@ public class Main {
 		while(true) {
 			System.out.println("Digite 6 números para o seu bilhete(entre 1 a 60)");
 			
+			int[] numerosEscolhidos = new int[6];
+			for(int i = 0; i < numerosEscolhidos.length; i++) {
+				System.out.println("Digite o " + (i+1) + "º número: ");
+				numerosEscolhidos[i] = sc.nextInt();
+			}
+			
+			Bilhete bilhete = new Bilhete(numerosEscolhidos);
+			
+			bilhetes.add(bilhete);
+			
 			System.out.println("Deseja registrar outro bilhete? (s / n)");
-			String resposta = sc.next();
+			String resposta = sc.next();			
 			
 			if(resposta.equals("n")) {
 				break;
 			}
+		}
+		
+		System.out.println("Realizando sorteio... ");
+		for(Bilhete b : bilhetes) {
+			b.realizarSorteio();
+			b.exibirResultados();
 		}
 		
 		sc.close();
